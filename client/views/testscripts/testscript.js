ed9626a9916140e2bb272913c402431fea9021b0
Korey Kassir <kkassir@popsugar.com>
2014-01-14 16:59:11 -0800

    can now delete and edit testscripts

--- client/views/testscripts/testscript.js
+++ client/views/testscripts/testscript.js
@@ -4,5 +4,25 @@ Template.testscript.events({
 			$(e.currentTarget).find('.invisible').show();
 			$(e.currentTarget).find('.testscript-steps').hide()
 		}
+	},
+	'click .btn-new-testscript.edit.update': function(e) {
+		if (Meteor.user()) {
+			$(e.currentTarget).find('.invisible').hide();
+			$(e.currentTarget).find('.testscript-steps').show()
+			var steps = $(e.currentTarget).siblings('textarea').val();
+			var id = this._id;
+			Testscripts.update(id, { $set: { steps: steps }});
+		}
+	},
+	'click .btn-new-testscript.edit.delete': function(e) {
+		if (Meteor.user()) {
+			$(e.currentTarget).find('.invisible').hide();
+			$(e.currentTarget).find('.testscript-steps').show()
+
+			if (confirm("Delete this testscript?")) {
+				var id = this._id;
+				Testscripts.remove(id);
+			}
+		}
 	}
 })
