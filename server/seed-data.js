27a1219940e912dbf596b14c0f447c24c26659a4
Korey Kassir <kkassir@popsugar.com>
2015-02-18 14:19:18 -0800

    Added try catch around seed data

--- server/seed-data.js
+++ server/seed-data.js
@@ -1,3 +1,8 @@
 userAdmins = Meteor.settings.userAdmins;
 
-Meteor.users.update({username: {$in: userAdmins}}, {$set: {isAdmin: true}}, {multi: true});
+try {
+  Meteor.users.update({username: {$in: userAdmins}}, {$set: {isAdmin: true}}, {multi: true});
+}
+catch (error) {
+
+}
