a20fd89f3802d017ae6af93052003f4a4154b35c
Korey Kassir <kkassir@popsugar.com>
2015-02-19 11:22:27 -0800

    added hack reset password

--- collections/users.js
+++ collections/users.js
@@ -1,5 +1,13 @@
 Meteor.methods({
   deleteUser: function(user) {
     Meteor.users.remove({username: user});
+  },
+  resetUserPassword: function(username, newPassword) {
+    if (Meteor.user().isAdmin) {
+      var user = Meteor.users.findOne({username: username});
+      if (user) {
+        Accounts.setPassword(user["_id"], newPassword);
+      }
+    }
   }
 });
